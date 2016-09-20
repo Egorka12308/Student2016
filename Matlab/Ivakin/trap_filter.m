b1caef99224fe5569cd362afc5798ceb8f6f07e3
Minhir <vl.ivakin@yandex.ru>
2016-09-20 13:06:29 +0700

    Update filter

--- Matlab/Ivakin/trap_filter.m
+++ Matlab/Ivakin/trap_filter.m
@@ -1,29 +1,29 @@
 function y = trap_filter(i, k, l, t1, t2, M)
 % задаём каждую функцию фильтра
-    function y = r(i, k, l, t1, t2, M)
-        y = p(i, k, l, t1, t2, M) + M * d(i, k, l, t1, t2);
+    function y = r(i)
+        y = p(i) + M * d(i);
     end
-    function y = s(i, k, l, t1, t2, M)
+    function y = s(i)
         if i < 0
             y = 0;
         else
-            y = s(i - 1, k, l, t1, t2, M) + r(i, k, l, t1, t2, M);
+            y = s(i - 1) + r(i);
         end
     end
-    function y = d(i, k, l, t1, t2)
+    function y = d(i)
         if i < 0
             y = 0;
         else
             y = func(i, t1, t2) - func(i - k, t1, t2) - func(i - l, t1, t2) + func(i - k - l, t1, t2);
         end
     end
-    function y = p(i, k, l, t1, t2, M)
+    function y = p(i)
         if i < 0
             y = 0;
         else
-            y = p(i - 1, k, l, t1, t2, M) + d(i, k, l, t1, t2);
+            y = p(i - 1) + d(i);
         end
     end
 % считаем значение, которое даст фильтр
-    y = s(i, k, l, t1, t2, M);
+    y = s(i);
 end
